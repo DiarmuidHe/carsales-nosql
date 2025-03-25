681a35adad3d3092a3a36b85f22635ae5cc1f4ca
DiarmuidHe <s00251204@atu.ie>
2025-03-25 21:04:42 +0000

    fixing bug issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CarlistComponent } from "./componets/carlist/carlist.component";
+import { CarComponent } from "./componets/car/car.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CarlistComponent],
+  imports: [RouterOutlet, CarlistComponent, CarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
